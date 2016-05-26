db5918dce9178dddbdcc3f46b966cb22a879fe69
Dawid Marszalkiewicz <leh.16.dm@gmail.com>
2016-05-26 19:44:08 +0200

    Przykład wywolania feedforward

--- ann.py
+++ ann.py
@@ -3,7 +3,6 @@
 
 import numpy as np
 import random
-import mnistHandwriting as mh
 
 class NeuralNet(object):
 
@@ -29,73 +28,133 @@ class NeuralNet(object):
 
     def __init__ (self, sizes):
         #Hyper-parameters
-        self.layers = len(sizes)
+        self.num_layers = len(sizes)
         self.sizes = sizes
-        self.weights = [np.random.randn(self.sizes[i], self.sizes[i+1]) for i in range(0,(self.layers)-1)]
+        self.biases = [np.random.randn(y, 1) for y in sizes[1:]]
+        self.weights = [np.random.randn(y, x) for x, y in zip(sizes[:-1], sizes[1:])]
 
 
-    ## Metoda aktywacji neuronu sigmoidalnego:
-    # y = 1 / (1+exp(- iloczyny wszystkich wag i wejść))
-    # zwraca activity i-tej warstwy połączeń
-    # i liczone od zera
+    ## Funkcja aktywacji neuronu sigmoidalnego:
+    # y = 1 / (1+exp(- suma iloczynów wszystkich wag i wejść - bias neuronu))
     #
-    #\param[in] inpt macierz wejściowa warstwy neuronów
-    #\param[in] i warstwa połączeń (indeks od 0)
-    #\retval activity Aktywność warstwy i
-    #
-    # np.dot(x, y) - mnozenie macierzy
-    def NeuronActivation(self,inpt,i):
-        return self.sigmoid(np.dot(inpt, self.weights[i]))
+    # zip(a, b) - tworzy iterator pozwalajacy na przejscie po kolejnych elementach a i b
+    #             i wykonac operacje i-tego a z i-tym b. Zatrzymuje sie, gdy jedna ze struktur
+    #             (a lub b) sie skonczy
+    # dot(x, y) - mnozenie macierzy
+    def NeuronActivation (self,inpt):
+        for wght in self.weights:
+            a = sigmoid(np.dot(wght, inpt))
+        return a
 
+    ## Funkcja obliczajaca wyjscie sieci neuronowej dla zadanego wejscia
+    def feedforward(self, a):
+        for b, w in zip(self.biases, self.weights):
+            a = sigmoid(np.dot(w, a) + b)
+        return a
 
-    ## Metoda przeprowadzająca propagację do przodu
-    #\param[in] lista lista list z danymi wejściowymi
-    #
-    #\warning każda lista wewnętrzna danych wejściowych musi zawierać tyle elementów,
-    #\warning ile neuronów wejściowych posiada sieć
-    def forwardPropagation(self,inpt):
-        an = inpt
-        for i in range((self.layers-1)):
-            an = self.NeuronActivation(an,i)
-        return an
+### Kod z neuralnetworksanddeeplearning
+    def SGD(self, training_data, epochs, mini_batch_size, eta, test_data=None):
+        if test_data: n_test = len(test_data)
+        n = len(training_data)
+        for j in xrange(epochs):
+            random.shuffle(training_data)
+            mini_batches = [
+                training_data[k:k+mini_batch_size]
+                for k in xrange(0, n, mini_batch_size)]
+            for mini_batch in mini_batches:
+                self.update_mini_batch(mini_batch, eta)
+            if test_data:
+                print "Epoch {0}: {1} / {2}".format(
+                    j, self.evaluate(test_data), n_test)
+            else:
+                print "Epoch {0} complete".format(j)
 
-    ## Metoda wyliczająca funkcję kosztów
-    def costFunction(self, inpt, targt):
-        self.outp_estim = self.forwardPropagation(inpt)
-        J = 0.5*sum((targt-self.outp_estim)**2)
-        return J
+    def update_mini_batch(self, mini_batch, eta):
+        """Update the network's weights and biases by applying
+        gradient descent using backpropagation to a single mini batch.
+        The ``mini_batch`` is a list of tuples ``(x, y)``, and ``eta``
+        is the learning rate."""
+        nabla_b = [np.zeros(b.shape) for b in self.biases]
+        nabla_w = [np.zeros(w.shape) for w in self.weights]
+        for x, y in mini_batch:
+            delta_nabla_b, delta_nabla_w = self.backprop(x, y)
+            nabla_b = [nb+dnb for nb, dnb in zip(nabla_b, delta_nabla_b)]
+            nabla_w = [nw+dnw for nw, dnw in zip(nabla_w, delta_nabla_w)]
+        self.weights = [w-(eta/len(mini_batch))*nw
+                        for w, nw in zip(self.weights, nabla_w)]
+        self.biases = [b-(eta/len(mini_batch))*nb
+                       for b, nb in zip(self.biases, nabla_b)]
 
-    ## funkcja sigmoidalna
-    # \retval sigmoid wynik
-    def sigmoid(self, exponent):
-        return 1.0/(1.0+np.exp(-exponent))
+    def backprop(self, x, y):
+        """Return a tuple ``(nabla_b, nabla_w)`` representing the
+        gradient for the cost function C_x.  ``nabla_b`` and
+        ``nabla_w`` are layer-by-layer lists of numpy arrays, similar
+        to ``self.biases`` and ``self.weights``."""
+        nabla_b = [np.zeros(b.shape) for b in self.biases]
+        nabla_w = [np.zeros(w.shape) for w in self.weights]
+        # feedforward
+        activation = x
+        activations = [x] # list to store all the activations, layer by layer
+        zs = [] # list to store all the z vectors, layer by layer
+        for b, w in zip(self.biases, self.weights):
+            z = np.dot(w, activation)+b
+            zs.append(z)
+            activation = sigmoid(z)
+            activations.append(activation)
+        # backward pass
+        delta = self.cost_derivative(activations[-1], y) * \
+            sigmoid_prime(zs[-1])
+        nabla_b[-1] = delta
+        nabla_w[-1] = np.dot(delta, activations[-2].transpose())
+        # Note that the variable l in the loop below is used a little
+        # differently to the notation in Chapter 2 of the book.  Here,
+        # l = 1 means the last layer of neurons, l = 2 is the
+        # second-last layer, and so on.  It's a renumbering of the
+        # scheme in the book, used here to take advantage of the fact
+        # that Python can use negative indices in lists.
+        for l in xrange(2, self.num_layers):
+            z = zs[-l]
+            sp = sigmoid_prime(z)
+            delta = np.dot(self.weights[-l+1].transpose(), delta) * sp
+            nabla_b[-l] = delta
+            nabla_w[-l] = np.dot(delta, activations[-l-1].transpose())
+        return (nabla_b, nabla_w)
 
-    ##Pochodna funkcji sigmoidalnej
-    # \retval derivative_sigmoid wynik
-    def derivative_sigmoid(self,exponent):
-        return np.exp(-exponent)/((1+np.exp(-exponent))**2)
+    def evaluate(self, test_data):
+        """Return the number of test inputs for which the neural
+        network outputs the correct result. Note that the neural
+        network's output is assumed to be the index of whichever
+        neuron in the final layer has the highest activation."""
+        test_results = [(np.argmax(self.feedforward(x)), y)
+                        for (x, y) in test_data]
+        return sum(int(x == y) for (x, y) in test_results)
 
+    def cost_derivative(self, output_activations, y):
+        """Return the vector of partial derivatives \partial C_x /
+        \partial a for the output activations."""
+        return (output_activations-y)
 
-# ##Test sieci
-# NN = NeuralNet([784,30,10])
-# #print NN.weights
-# #print NN.layers
-# inp = mh.MNISTexample(0,1,only01=False)
-# # input1 = [0.2, 0.4]
-# # input2 = [0.3, 0.5]
-# # inp = [input1, input2]
-# print "Input:"
-# print inp
-# val = NN.forwardPropagation(inp[0][0])
-# print "\n"
-# print val
+#### Miscellaneous functions
+def sigmoid(z):
+    """The sigmoid function."""
+    return 1.0/(1.0+np.exp(-z))
 
-#
-#     ## Funkcja zapisująca nauczone wagi
-#     def saveWeights(self):
-#         return 0
-#
-#
-# ##Funkcja wczytująca sieć
-# def NetworkInitFromFile():
-#     return 0
+def sigmoid_prime(z):
+    """Derivative of the sigmoid function."""
+    return sigmoid(z)*(1-sigmoid(z))
+
+import mnistHandwriting as mh
+import mnist_loader
+# Wczytanie danych (troche to trwa) oraz inicjalizacja sieci
+training_data, validation_data, test_data =  mnist_loader.load_data_wrapper()
+NN = NeuralNet ([784, 30, 10])
+#  Szybki trening
+NN.SGD(training_data, 1, 10, 3.0, test_data = test_data )
+# Sprawdzenie, do feedforward przekazujemy tylko sam obrazek w formie dlugiego
+# wektora kolumny, wyswietlane wyjscie
+print NN.feedforward(test_data[0][0])
+#  Tutaj sa wyswietlane prawidlowe wyniki
+print test_data[0][1]
+
+#  Są to te samo abrazki co pobiera mnistHandwriting, pod indeksem 0 rzeczywiscie jest 7
+mh.writeMNISTimage(mh.MNISTexample (0,2, bTrain = False, only01 = False))
